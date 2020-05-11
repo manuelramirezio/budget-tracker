851c967591a7ef65efe269c2c5671db2355a10b5
Full Stack <fullstack@fullstacks-MacBook-Pro.local>
2020-05-11 14:59:35 -0400

    Adding code ready and running

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://manuel:manuel12345@ds145128.mlab.com:45128/heroku_z1lx5v1h", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
